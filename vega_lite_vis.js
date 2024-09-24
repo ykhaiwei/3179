679547af5269a7c047aa0fa04997db8e9bb7d410
Khai Wei <121887080+ykhaiwei@users.noreply.github.com>
2024-09-24 18:40:12 +0800

    Add world map visualization files

---
+++ vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "choropleth_map.vg.json";
+vegaEmbed("#choropleth_map", vg_1)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
